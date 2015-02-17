51ba9b287a49f22d2647de99c2380da7a8111cf3
eyob-- <tse.eyob@gmail.com>
2015-02-17 13:53:46 -0500

    Finished main framework of HeapModus.Heap
    Completed all methods inside HeapModus.Heap except for those dealing with animation. HeapModus.Heap is heavily modeled after TreeModus.Tree and many methods are identical. HeapModus.Heap stores sylladex cards in a max heap and functions as a tree in all other aspects (except for the fact that it doesn't need to balance itself).

--- modus-dev/src/HeapModus.java
+++ modus-dev/src/HeapModus.java
@@ -1,4 +1,6 @@
+import java.awt.Point;
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import sylladex.FetchModus;
@@ -70,27 +72,52 @@ public class HeapModus extends FetchModus {
 	public class Heap implements Iterable<Heap.Node>, Iterator<Heap.Node> {
 		
 		private Node root;
+		private ArrayList<Node> visited = new ArrayList<Node>();
 		
 		public Heap() {
-			
+			root = null;
 		}
 
 		@Override
 		public boolean hasNext() {
-			// TODO Auto-generated method stub
+			for (SylladexCard card : deck.getCards()) {
+				Node node = getNodeWithCard(card);
+				// if there's no root, why are you iterating???
+				if (node == null) break;
+				else if (!visited.contains(node))
+					return true;
+			}
+			visited.clear();
 			return false;
 		}
 
 		@Override
 		public Node next() {
-			// TODO Auto-generated method stub
-			return null;
+			if (visited.size() == 0) {
+				visited.add(root);
+				return root;
+			}
+			while (true) {	// The danger is real
+				Node lastVisited = visited.get(visited.size()-1);
+				// if we haven't been to the left of the last node and there's a node there
+				if (visited.contains(lastVisited.left) && lastVisited.left != null) {
+					visited.add(lastVisited.left);
+					return lastVisited.left;
+				}
+				// ditto for the right
+				else if(visited.contains(lastVisited.right) && lastVisited.right != null) {
+					visited.add(lastVisited.right);
+					return lastVisited.right;
+				}
+				// if we've visited both children, go back to the parent of this node
+				else {
+					visited.add(lastVisited.parent);
+				}
+			}
 		}
 
 		@Override
 		public void remove() {
-			// TODO Auto-generated method stub
-			
 		}
 
 		@Override
@@ -98,8 +125,74 @@ public class HeapModus extends FetchModus {
 			return this;
 		}
 		
+		public Node getRoot() {
+			return root;
+		}
+		
+		public void clear() {
+			root = null;
+		}
+		
 		public void add(SylladexCard card) {
+			Node node = new Node(card);
+			if (root == null) {
+				root = node;
+				return;
+			}
 			
+			Node insertOn = nodeToInsertOn();	// "but doesn't that make it an O(n) insertion time?" yes. it does. got a problem?
+			if (insertOn.left == null) insertOn.left = node;
+			else insertOn.right = node;
+			
+			while (node.card.getItem().getName().toLowerCase().compareTo(node.parent.card.getItem().getName().toLowerCase()) > 0) {
+				node.card = node.parent.card;
+				node.parent.card = card;
+				node = node.parent;
+				if (node == root) return;
+			}
+		}
+		
+		/**
+		 * Returns the node which the next inserted node should be a child of. IMPORTANT: MAKE SURE ROOT IS NOT NULL WHEN YOU USE THIS!
+		 * @return parent of the next inserted node
+		 */
+		@SuppressWarnings("unchecked")
+		public Node nodeToInsertOn() {
+			ArrayList<Node> level = new ArrayList<Node>();
+			level.add(root);
+			if(root.left != null && root.right != null) return root;
+			while (true) {		// Oh god this is so dangerous...
+				ArrayList<Node> oldLevel = (ArrayList<Node>) level.clone();
+				level = new ArrayList<Node>();
+				for (Node n : oldLevel) {
+					level.add(n.left);
+					level.add(n.right);
+				}
+				for (Node n : level)
+					if (n.left == null || n.right == null) return n;
+			}
+		}
+		
+		public void remove(SylladexCard card) {
+			Node node = getNodeWithCard(card);
+			
+			// apparently we can only remove leaves (laaaaaaaame)
+			if (!node.isLeaf()) return;
+			
+			if (node.parent != null) {
+				if (node == node.parent.left) node.parent.left = null;
+				else node.parent.right = null;
+			}
+		}
+		
+		public Node getNodeWithCard(SylladexCard card) {
+			if (root != null) return root.getNodeWithCard(card);
+			return null;
+		}
+		
+		public void buildAnimation(SylladexCard card) {
+			card.setLocation(new Point(0, 0));
+			root.buildAnimation(card);
 		}
 		
 		public class Node {
